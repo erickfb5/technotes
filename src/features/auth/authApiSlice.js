3afe514ce956a69770ee328d0e501578ad204e1f
Erick Brandao <brandaoerick@MacBook-Air.local>
2023-04-26 15:01:47 -0300

    fixed: data variable issue

--- src/features/auth/authApiSlice.js
+++ src/features/auth/authApiSlice.js
@@ -17,7 +17,7 @@ export const authApiSlice = apiSlice.injectEndpoints({
       }),
       async onQueryStarted(arg, { dispatch, queryFulfilled }) {
         try {
-          // const { data } = 
+          // const { data } =
           await queryFulfilled;
           // console.log(data);
           dispatch(logOut());
@@ -36,8 +36,7 @@ export const authApiSlice = apiSlice.injectEndpoints({
       }),
       async onQueryStarted(arg, { dispatch, queryFulfilled }) {
         try {
-          // const { data } = 
-          await queryFulfilled;
+          const { data } = await queryFulfilled;
           // console.log(data);
           const { accessToken } = data;
           dispatch(setCredentials({ accessToken }));
